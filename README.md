15c997a994f28bdf9d119efcc8034ab5e97128df
diegodefuentes <diego.fuentes@softtek.com>
2015-09-29 09:49:13 -0500

    29 Sept 2015

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Cookbook para instalar y configurar Apache en Servidores
 tipo RHEL, CENTOS, Debian y Ubuntu
 
+Incluye soporte a modulo SSL para contemplar trafico encriptado
+
 Softtek
 
 Generado por Diego De Fuentes Ceballos
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright (c) 2015 Diego De Fuentes Ceballos,
 # Softtek All Rights Reserved.
 
-
+include_recipe 'selinux::disabled'
 include_recipe "#{cookbook_name}::server"
 include_recipe "#{cookbook_name}::mod_ssl"
 include_recipe "#{cookbook_name}::firewall"
